a0f883c6779b06a69ed9c42ef05d1b6d3e2858f2
anyakomkova <122331580+anyakomkova@users.noreply.github.com>
2023-04-22 13:28:29 -0500

    changed api key

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 // Initialize and add the map
 
-function initMap() {
- var map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 38.872, lng: -104.996},
-    zoom: 10
-  });
+let map;
 
-  var wasabiMarker = new google.maps.Marker({
-    position: {lat: 38.872, lng: -104.996},
-    map: map,
-    title: 'Wasabi Ramen Logan Square'
-  });
-}
+async function initMap() {
+    //@ts-ignore
+    const { Map } = await google.maps.importLibrary("map");
+  
+    map = new Map(document.getElementById("map"), {
+      center: {lat: 38.872, lng: -104.996},
+      zoom: 10,
+    });
 
+  }
+  
+  initMap();
