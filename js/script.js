904e3136eaf7d9de1cf02f9e7d854d2c4f653642
anyakomkova <122331580+anyakomkova@users.noreply.github.com>
2023-04-22 14:55:16 -0500

    troubleshooting the map

--- js/script.js
+++ js/script.js
@@ -3,19 +3,18 @@ let map;
 
 function initMap() {
   // The location of Wasabi Ramen
-  var wasabiMap = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
   // The map, centered at Wasabi Ramen
     zoom: 13,
-    center: {lat: 41.920119875987375, lng: -87.68949370382387},
+    center: {lat: 41.91964, lng: -87.69126},
   });
 
-  let marker = new google.maps.Marker({
-    position: {lat: 41.920119875987375, lng: -87.68949370382387},
-    map: wasabiMap,
+  const marker = new google.maps.Marker({
+    position: {lat: 41.91964, lng: -87.69126},
+    map: map,
     title: 'Wasabi Ramen'
   });
-
-  const location = [{lat: 41.920119875987375, lng: -87.68949370382387}, "Wasabi Ramen", "media/"],
+  
 }
 
 initMap();
