a122dda18fb255f07a78318feb3bec0ecad20133
anyakomkova <122331580+anyakomkova@users.noreply.github.com>
2023-04-22 14:57:39 -0500

    edited the location coordinates

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
   // The map, centered at Wasabi Ramen
     zoom: 13,
-    center: {lat: 41.91964, lng: -87.69126},
+    center: {lat: 41.91951, lng: -87.69151},
   });
 
   const marker = new google.maps.Marker({
-    position: {lat: 41.91964, lng: -87.69126},
+    position: {lat: 41.91951, lng: -87.69151},
     map: map,
     title: 'Wasabi Ramen'
   });
-  
+
 }
 
 initMap();
