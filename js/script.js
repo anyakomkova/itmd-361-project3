7df9da5fe358878ec675431eef249437a5bce883
anyakomkova <122331580+anyakomkova@users.noreply.github.com>
2023-04-22 13:35:46 -0500

    ttroubleshooting the map

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 // Initialize and add the map
-
 let map;
 
 async function initMap() {
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("map");
+
+    const wasabiRamen = {lat: 38.872, lng: -104.996};
+    const { map } = await google.maps.importLibrary("map");
+
+    // The map, centered at Wasabi Ramen
   
     map = new Map(document.getElementById("map"), {
-      center: {lat: 38.872, lng: -104.996},
+      center: wasabiRamen,
       zoom: 10,
+      mapId: "RAMEN_MAP",
     });
 
-  }
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: wasabiRamen,
+    title: "Wasabi",
+  });
+}
   
   initMap();
